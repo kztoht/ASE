b66f015b8beabbe35d8fcc8b60626e9643026b07
kztoht <kazuto.ohtsuki@gmail.com>
2024-05-24 17:44:09 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,18 +1,18 @@
-const textElement = document.getElementById("text");
-const feedbackElement = document.getElementById("feedback");
-
-const correctAnswers = [
-    "", "a", "a", "the", "the", "a", "a", "the", "the", "an", "a", "the", "a", "the", "the", "a", "the", "a", "a", "the", "the", "a", "the", "the", "the", "the"
-];
-
 function checkAnswers() {
-  let mistakes = false;
-  let feedback = "";
+    const dropdowns = document.querySelectorAll('.dropdown');
+    let correct = true;
+    let feedback = "";
 
-  const articleSelects = textElement.querySelectorAll("select[name='article']");
+    dropdowns.forEach(dropdown => {
+        if (dropdown.value !== dropdown.options[dropdown.selectedIndex].value) {
+            correct = false;
+            feedback += "Incorrect answer: " + dropdown.options[dropdown.selectedIndex].value + " should be selected. ";
+        }
+    });
 
-  articleSelects.forEach((select, index) => {
-    // Create the select element dynamically
-    const option = document.createElement("option");
-    option.value = "";
-    option.text
+    if (correct) {
+        feedback = "All answers are correct!";
+    }
+
+    document.getElementById('feedback').innerText = feedback;
+}
