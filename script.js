36c8a1ec8cfc5952c545fa0faf0ffd4676a5ddba
kztoht <kazuto.ohtsuki@gmail.com>
2024-05-24 16:18:00 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,39 +1,20 @@
-document.querySelectorAll('.dropdown').forEach(element => {
-  element.onclick = function() {
-    let currentText = element.textContent.replace(/[\[\]]/g, '');
-    let dropdown = createDropdown(currentText);
-    dropdown.onchange = function() {
-      element.textContent = '[' + this.value + ']';
-      element.appendChild(dropdown); // Hide dropdown after selection
-    };
-    element.innerHTML = '';
-    element.appendChild(dropdown);
-  };
-});
-
-function createDropdown(selected) {
-  let options = ['a', 'an', 'the', ''];
-  let select = document.createElement('select');
-  options.forEach(option => {
-    let opt = document.createElement('option');
-    opt.value = option;
-    opt.textContent = option || 'none';
-    if (option === selected) opt.selected = true;
-    select.appendChild(opt);
-  });
-  return select;
-}
-
 function checkAnswers() {
-  let answers = ['a', 'a', 'the', 'the', 'a', 'a', 'a', 'the', 'the', 'the', 'a', 'the', 'an', 'the', 'a', 'the', 'a', 'the'];
-  document.querySelectorAll('.dropdown').forEach((element, index) => {
-    let userAnswer = element.textContent.replace(/[\[\]]/g, '');
-    if (userAnswer === answers[index]) {
-      element.classList.add('correct');
-      element.classList.remove('incorrect');
-    } else {
-      element.classList.add('incorrect');
-      element.classList.remove('correct');
-    }
-  });
+    const answers = {
+        word1: "a",
+        word2: "a",
+        word3: "the",
+        word4: "the",
+        // Continue adding the correct answers for each dropdown ID
+    };
+
+    Object.keys(answers).forEach(function(id) {
+        const dropdown = document.getElementById(id);
+        if (dropdown.value === answers[id]) {
+            dropdown.classList.add('correct');
+            dropdown.classList.remove('incorrect');
+        } else {
+            dropdown.classList.add('incorrect');
+            dropdown.classList.remove('correct');
+        }
+    });
 }
