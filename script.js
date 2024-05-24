112fb81a66012e5210fe28b537eb18f8122c5c19
kztoht <kazuto.ohtsuki@gmail.com>
2024-05-24 17:37:10 +0900

    Update script.js

--- script.js
+++ script.js
@@ -15,28 +15,4 @@ function checkAnswers() {
     // Create the select element dynamically
     const option = document.createElement("option");
     option.value = "";
-    option.text = "--"; // Default option text
-    select.appendChild(option);
-
-    // Add other options based on correct answers
-    const options = ["a", "an", "the", ""]; // All possible options
-    options.forEach(choice => {
-      const option = document.createElement("option");
-      option.value = choice;
-      option.text = choice;
-      select.appendChild(option);
-    });
-
-    // Set the selected value based on user choice (if any)
-    select.value = select.options[select.selectedIndex].value;
-
-    // Check if the answer is correct
-    if (select.value !== correctAnswers[index]) {
-      mistakes = true;
-      select.style.backgroundColor = "pink"; // Highlight incorrect answer
-    }
-  });
-
-  feedback = mistakes ? "There are some mistakes. Please try again." : "All answers are correct!";
-  feedbackElement.textContent = feedback;
-}
+    option.text
