4bc334cce5cbd068f5e2d8af1a00f8bef47896b0
kztoht <kazuto.ohtsuki@gmail.com>
2024-05-24 16:34:28 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,28 @@
 function checkAnswers() {
+    // Object mapping dropdown IDs to their correct answers
     const answers = {
         word1: "a",
         word2: "a",
         word3: "the",
         word4: "the",
-        // Continue adding the correct answers for each dropdown ID
+        word5: "a",
+        word6: "a",
+        word7: "a",
+        word8: "the",
+        word9: "the",
+        word10: "the",
+        word11: "a",
+        word12: "the",
+        word13: "an",
+        word14: "the",
+        word15: "a",
+        word16: "the",
+        word17: "a",
+        word18: "the"
+        // Add more pairs if additional dropdowns are added
     };
 
-    Object.keys(answers).forEach(function(id) {
+    Object.keys(answers).forEach(id => {
         const dropdown = document.getElementById(id);
         if (dropdown.value === answers[id]) {
             dropdown.classList.add('correct');
