a410d0a8c824e2ae4a02c1650c3f7aa625d77bc2
kztoht <kazuto.ohtsuki@gmail.com>
2024-05-24 17:31:16 +0900

    Update script.js

--- script.js
+++ script.js
@@ -2,11 +2,13 @@ const textElement = document.getElementById("text");
 const feedbackElement = document.getElementById("feedback");
 
 const correctAnswers = [
-    "a", "a", "the", "the", "a", "a", "the", "the", "an", "a", "the", "a", "the", "the", "a", "the", "a", "a", "the", "the", "a", "the", "the", "the", "the"
+    "", "a", "a", "the", "the", "a", "a", "the", "the", "an", "a", "the", "a", "the", "the", "a", "the", "a", "a", "the", "the", "a", "the", "the", "the", "the"
 ];
 
 function checkAnswers() {
   let mistakes = false;
   let feedback = "";
 
-  const articleSelects = textElement.querySelectorAll("[name='
+  const articleSelects = textElement.querySelectorAll("select[name='article']");
+
+  articleSelects.forEach((select, index) =>
